8e3e0cd42f404ed7679cd1956c3eb9d500e8ac26
Glitch (mmcc-next-streamr) <none>
2020-03-31 00:37:12 +0000

    🎐🏠 Checkpoint ./pages/index.js:1834082/1

--- pages/index.js
+++ pages/index.js
@@ -60,7 +60,7 @@ export default () => {
   const startStreaming = () => {
     setStreaming(true);
 
-    wsRef.current = new WebSocket(`ws://${window.location.host}/rtmp?key=${streamKey}`);
+    wsRef.current = new WebSocket(`wss://${window.location.host}/rtmp?key=${streamKey}`);
 
     wsRef.current.addEventListener('open', function open() {
       setConnected(true);
