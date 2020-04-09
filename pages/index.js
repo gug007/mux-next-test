cac25af33e192c6044e94c9b7f5c394280847493
Glitch (mmcc-next-streamr) <none>
2020-04-09 02:32:24 +0000

    🍎📖 Checkpoint ./pages/index.js:1834082/1103

--- pages/index.js
+++ pages/index.js
@@ -112,7 +112,9 @@ export default () => {
 
   return (
     <div style={{ maxWidth: '980px', margin: '0 auto' }}>
-      <Head>
+      <a href="https://glitch.com/edit/?utm_content=project_mmcc-next-streamr&utm_source=remix_this&utm_medium=button&utm_campaign=glitchButton#!/remix/mmcc-next-streamr" style={{position: 'absolute', top: '10px', right: '10px'}}>
+        <img src="https://cdn.glitch.com/2bdfb3f8-05ef-4035-a06e-2043962a3a13%2Fremix%402x.png?1513093958726" alt="remix this" height="33" />
+      </a><Head>
         <title>Streamr</title>
       </Head>
       <h1>Streamr</h1>
